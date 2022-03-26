59d5e989394c67eca800dfd313d4cfefae607c1d
Aniruddh Sharma <aniruddhsharmajpr@gmail.com>
2022-03-26 14:17:05 +0530

    1st code

---
+++ Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,51 @@
+// Method:
+//
+// * Firstly we find the average of high and low and save it in a variable k
+// * Case 1:
+//      * when both high and low are even
+//      * In this case ans would be directly the average which is k here
+//      * Example:
+//          * high =6 low = 2
+//          * k = 2 (numbers are 3,5)
+// * Case 2:
+//      * When one of the high or low is odd and other is even
+// * Case 3:
+//      * When both high and low are odd
+// * For Case 2 and 3:
+//      * we just have to account for the one extra odd value
+//      * so we check if it is the 2nd or 3rd case
+//      * If it is then add 1 to k
+//      * Example:
+//              * low = 3 high =6
+//              * k = 1+1 =2 (numbers: 3,5)
+//              * low = 2 high =5
+//              * k = 1+1 =2 (numbers: 3,5)
+//              * low = 3 high =9
+//              * k = 3+1 =2 (numbers: 3,5,7,9)
+// * Thus all cases are handled
+// * Finally, return k
+
+// * Time Complexity: O(1)
+//          * As, we are only finding an average between two given numbers
+//          * Hence, constant Time complexity
+// * Space Complexity: O(1)
+//          * As, we have created only one extra variable to store the result
+//          * Hence, constant Space complexity
+
+// Code:
+
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+        int k = (high - low) / 2;
+        if (high % 2 != 0 || low % 2 != 0)
+        {
+            k += 1;
+        }
+        return k;
+    }
+};
+
+// Happy Coding :-)
