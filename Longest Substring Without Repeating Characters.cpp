c073cb5f7669f11c28b1da6111375fc37faae7a2
Aniruddh Sharma <aniruddhsharmajpr@gmail.com>
2022-06-10 12:01:14 +0530

    codes

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -4,36 +4,26 @@ public:
     int lengthOfLongestSubstring(string s)
     {
         int n = s.length();
-        if (n == 0)
+        set<int> st;
+        int i = 0; // start of the window
+        int j = 0; // last point in the window
+        int k = 0; // length of the window
+        while (i < n && j < n)
         {
-            return 0;
-        }
-        unordered_map<char, int> mp;
-        int i = 0;
-        int j = 0;
-        int m = INT_MIN;
-        while (j < n)
-        {
-            mp[s[j]]++;
-            if (mp.size() == (j - i + 1))
+            if (st.find(s[j]) == st.end()) // if the element is not in the set
             {
-                m = max(m, (j - i + 1));
-                j++;
+                st.insert(s[j++]); // insert the element and move the j to the next element
+                k = max(k, j - i); // update the length
             }
-            else if (mp.size() < (j - i + 1))
+            else
             {
-                while (mp.size() < (j - i + 1))
-                {
-                    mp[s[i]]--;
-                    if (mp[s[i]] == 0)
-                    {
-                        mp.erase(s[i]);
-                    }
-                    i++;
-                }
-                j++;
+                /*
+                If character does exist in the set, ie. it is a repeated character,
+                we update the left side counter i, and continue with the checking for substring.
+                */
+                st.erase(s[i++]);
             }
         }
-        return m;
+        return k;
     }
 };
